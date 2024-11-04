e5943a49634041435a919674bcd101156c193794
jakjus <fazzebo@gmail.com>
2024-11-04 18:18:04 +0900

    a

--- src/foul.ts
+++ src/foul.ts
@@ -51,14 +51,11 @@ const handleSlide = (slider: PlayerAugmented, victim: PlayerAugmented) => {
 		cardsFactor += 0.3
 	}
 	const power = Math.sqrt((sliderProps.xspeed-victimProps.xspeed)**2+(sliderProps.yspeed-victimProps.yspeed)**2)*0.7
-	console.log('power', power)
 	const slowdown = power > 2.7 ? 0.05*power : 0.04*power
-	console.log('slowdown', slowdown)
 	const av = power > 2.7 ? '❌' : '🩹'
 	room.setPlayerAvatar(victim.id, av)
 	victim.slowdown = slowdown
 	victim.slowdownUntil = new Date().getTime()+1000*(0.1*power**8*(0.5+0.5*Math.random()*Math.random()))
-	console.log('slowdownuntil', 1000*(0.06*power**8*(0.5+0.5*Math.random()*Math.random())))
 	victim.canCallFoulUntil = new Date().getTime()+4000
 	sendMessage('You have been fouled. You can call foul by holding X in the next 4 seconds.', victim)
 	slider.foulsMeter += 0.8*power*cardsFactor*(Math.random()*0.2+0.9)
