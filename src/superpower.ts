f76d8cef02d13b63d7869bcbf0621efea3e13a81
jakjus <fazzebo@gmail.com>
2024-11-05 15:16:42 +0900

    o

--- src/superpower.ts
+++ src/superpower.ts
@@ -125,8 +125,8 @@ const finKickOrSlide = (game: Game, p: PlayerAugmented) => {
 	const vecX = xx/magnitude
 	const vecY = yy/magnitude
 	const dir = {x: vecX, y: vecY}
-	const totalXspeed = ((dir.x+props.xspeed*0.4)*(activationRange-dist)**0.2)*4.5
-	const totalYspeed = ((dir.y+props.yspeed*0.4)*(activationRange-dist)**0.2)*4.5
+	const totalXspeed = ((dir.x+props.xspeed*0.4)*(activationRange-dist)**0.2)*4.9
+	const totalYspeed = ((dir.y+props.yspeed*0.4)*(activationRange-dist)**0.2)*4.9
 	room.setDiscProperties(0, {
 		xspeed: totalXspeed,
 		yspeed: totalYspeed
